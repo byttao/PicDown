45fd7d921e6822dce1d5a0fb49a341acf727fb02
byttao <byttao@gmail.com>
2025-09-30 22:30:03 +0800

    V1.2

---
+++ PicDown/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace PicDown
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            tableLayoutPanel1 = new TableLayoutPanel();
+            rtbHtml = new RichTextBox();
+            richTextBoxLog = new RichTextBox();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            label1 = new Label();
+            txtOutputFolder = new TextBox();
+            btnFloder = new Button();
+            pictureBox1 = new PictureBox();
+            btnDown = new Button();
+            richTextBox1 = new RichTextBox();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 100F));
+            tableLayoutPanel1.Controls.Add(rtbHtml, 0, 0);
+            tableLayoutPanel1.Controls.Add(richTextBoxLog, 0, 2);
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 1);
+            tableLayoutPanel1.Controls.Add(pictureBox1, 1, 0);
+            tableLayoutPanel1.Controls.Add(btnDown, 1, 1);
+            tableLayoutPanel1.Controls.Add(richTextBox1, 1, 2);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.Padding = new Padding(10);
+            tableLayoutPanel1.RowCount = 3;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle());
+            tableLayoutPanel1.Size = new Size(882, 844);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // rtbHtml
+            // 
+            rtbHtml.Dock = DockStyle.Fill;
+            rtbHtml.Location = new Point(13, 13);
+            rtbHtml.Name = "rtbHtml";
+            rtbHtml.Size = new Size(756, 240);
+            rtbHtml.TabIndex = 0;
+            rtbHtml.Text = "";
+            // 
+            // richTextBoxLog
+            // 
+            richTextBoxLog.Dock = DockStyle.Fill;
+            richTextBoxLog.Location = new Point(13, 309);
+            richTextBoxLog.Name = "richTextBoxLog";
+            richTextBoxLog.ReadOnly = true;
+            richTextBoxLog.Size = new Size(756, 522);
+            richTextBoxLog.TabIndex = 1;
+            richTextBoxLog.Text = "";
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 3;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 110F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 70F));
+            tableLayoutPanel2.Controls.Add(label1, 0, 0);
+            tableLayoutPanel2.Controls.Add(txtOutputFolder, 1, 0);
+            tableLayoutPanel2.Controls.Add(btnFloder, 2, 0);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(13, 259);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel2.Size = new Size(756, 44);
+            tableLayoutPanel2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Location = new Point(39, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 17);
+            label1.TabIndex = 0;
+            label1.Text = "保存目录：";
+            // 
+            // txtOutputFolder
+            // 
+            txtOutputFolder.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            txtOutputFolder.BorderStyle = BorderStyle.FixedSingle;
+            txtOutputFolder.Location = new Point(113, 10);
+            txtOutputFolder.Name = "txtOutputFolder";
+            txtOutputFolder.ReadOnly = true;
+            txtOutputFolder.Size = new Size(570, 23);
+            txtOutputFolder.TabIndex = 1;
+            txtOutputFolder.TextChanged += txtOutputFolder_TextChanged;
+            // 
+            // btnFloder
+            // 
+            btnFloder.Anchor = AnchorStyles.Left;
+            btnFloder.Location = new Point(689, 10);
+            btnFloder.Name = "btnFloder";
+            btnFloder.Size = new Size(26, 23);
+            btnFloder.TabIndex = 2;
+            btnFloder.Text = "...";
+            btnFloder.UseVisualStyleBackColor = true;
+            btnFloder.Click += btnFloder_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Dock = DockStyle.Fill;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(775, 13);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(94, 240);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // btnDown
+            // 
+            btnDown.Anchor = AnchorStyles.None;
+            btnDown.Location = new Point(778, 261);
+            btnDown.Name = "btnDown";
+            btnDown.Size = new Size(88, 40);
+            btnDown.TabIndex = 2;
+            btnDown.Text = "图包下载";
+            btnDown.UseVisualStyleBackColor = true;
+            btnDown.Click += btnDown_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.BackColor = Color.White;
+            richTextBox1.BorderStyle = BorderStyle.None;
+            richTextBox1.Dock = DockStyle.Fill;
+            richTextBox1.Location = new Point(775, 309);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ReadOnly = true;
+            richTextBox1.Size = new Size(94, 522);
+            richTextBox1.TabIndex = 5;
+            richTextBox1.Text = "V1.2 250927\n更新：视频下载\n\nV1.1 250927\n更新：SKU图下载\n\nV1.0 250926\n更新：主图、详情图下载";
+            // 
+            // Form1
+            // 
+            AcceptButton = btnDown;
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(882, 844);
+            Controls.Add(tableLayoutPanel1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "图包下载器 V1.2 250927";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private RichTextBox rtbHtml;
+        private RichTextBox richTextBoxLog;
+        private Button btnDown;
+        private TableLayoutPanel tableLayoutPanel2;
+        private Label label1;
+        private TextBox txtOutputFolder;
+        private Button btnFloder;
+        private PictureBox pictureBox1;
+        private RichTextBox richTextBox1;
+    }
+}
---
+++ PicDown/Form1.cs
@@ -0,0 +1,467 @@
+using System.Text.RegularExpressions;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using Color = System.Drawing.Color;
+
+// 确保你安装了 ImageSharp
+// 引入 Jpeg 格式
+using Image = SixLabors.ImageSharp.Image;
+using HtmlDocument = HtmlAgilityPack.HtmlDocument; // 引入 Webp 格式
+
+namespace PicDown
+{
+    public partial class Form1 : Form
+    {
+        private const string ImageSrcRegexPattern = @"<img\s+(?:[^>]*?)src=[""']?([^""'\s>]+)[""']?(?:[^>]*?)>";
+        private string defaultProtocol = "https";
+        private string downloadDirectory;
+
+        private string fullHtmlContent;
+
+        public Form1()
+        {
+            InitializeComponent();
+            txtOutputFolder.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        }
+
+        private void LogMessage(string message, LogLevel level)
+        {
+            // 在 UI 线程上更新 RichTextBox
+            if (richTextBoxLog.InvokeRequired)
+            {
+                richTextBoxLog.Invoke(() => AppendLog(message, level));
+            }
+            else
+            {
+                AppendLog(message, level);
+            }
+        }
+
+        private void AppendLog(string message, LogLevel level)
+        {
+            string prefix = "";
+            Color color = Color.Black; // 默认颜色
+
+            switch (level)
+            {
+                case LogLevel.Info:
+                    prefix = "[INFO] ";
+                    color = Color.Blue;
+                    break;
+
+                case LogLevel.Warning:
+                    prefix = "[WARN] ";
+                    color = Color.Orange;
+                    break;
+
+                case LogLevel.Error:
+                    prefix = "[ERROR] ";
+                    color = Color.Red;
+                    break;
+
+                case LogLevel.Success:
+                    prefix = "[SUCCESS] ";
+                    color = Color.Green;
+                    break;
+
+                case LogLevel.Processing:
+                    prefix = "[PROCESS] ";
+                    color = Color.Purple;
+                    break;
+            }
+
+            // 格式化输出
+            string formattedMessage = $"{DateTime.Now:HH:mm:ss} {prefix}{message}\r\n";
+
+            // 记录颜色
+            int startIndex = richTextBoxLog.Text.Length;
+            richTextBoxLog.AppendText(formattedMessage);
+            int endIndex = richTextBoxLog.Text.Length;
+
+            richTextBoxLog.Select(startIndex, endIndex - startIndex);
+            richTextBoxLog.SelectionColor = color;
+            richTextBoxLog.SelectionCharOffset = 0; // 恢复默认偏移
+            richTextBoxLog.DeselectAll(); // 取消选择
+
+            // 自动滚动到底部
+            richTextBoxLog.ScrollToCaret();
+        }
+
+        private void Convert(string webpFilePath, string jpgFilePath)
+        {
+            if (!File.Exists(webpFilePath))
+            {
+                LogMessage($"    -> WebP文件没有找到: {webpFilePath}", LogLevel.Error);
+            }
+
+            try
+            {
+                // 1. 使用 ImageSharp 加载 WebP 图片
+                using (var image = Image.Load(webpFilePath))
+                {
+                    // 2. 保存为 JPG 格式
+                    // SaveAsJpeg 方法需要指定 JpegEncoder，可以设置一些参数，例如质量
+                    image.SaveAsJpeg(jpgFilePath, new JpegEncoder
+                    {
+                        Quality = 90
+                    }); // Quality 范围 0-100
+                }
+
+                if (File.Exists(webpFilePath))
+                {
+                    File.Delete(webpFilePath);
+                }
+
+                LogMessage($"    -> 已成功转换保存: {webpFilePath} 到 {jpgFilePath}", LogLevel.Success);
+            }
+            catch (Exception ex)
+            {
+                LogMessage($"    [!] 处理错误 - {webpFilePath}: {ex.Message}", LogLevel.Error);
+            }
+        }
+
+        private string GetIDFromUrl()
+        {
+            // 要使用的正则表达式
+            string pattern = @"(?<=data-item\s*=\s*"")\d+(?="")";
+
+            // 查找匹配项
+            Match match = Regex.Match(fullHtmlContent, pattern);
+
+            // 检查是否找到匹配项
+            if (match.Success)
+            {
+                LogMessage($"    -> 当前链接ID为: {match.Value}", LogLevel.Success);
+                // 找到了第一个匹配项，获取其值
+                return match.Value;
+            }
+
+            return "UnknownID";
+        }
+
+        private List<(string Url, int Index)> ExtractVedioLinksFromDiv(string elementId)
+        {
+            var processedVedioLinks = new List<(string Url, int Index)>();
+            var doc = new HtmlDocument();
+            doc.LoadHtml(fullHtmlContent);
+            // 3. 使用 XPath 定位 video 元素
+            // XPath 语法：//tagName[@attributeName='attributeValue']
+            // //video[@id='yourElementId']
+            var videoNode = doc.DocumentNode.SelectSingleNode($"//video[@id='{elementId}']");
+
+            // 4. 检查是否找到元素并获取 src 属性
+            if (videoNode != null)
+            {
+                // 获取 src 属性的值
+                var srcAttribute = videoNode.Attributes["src"];
+                if (srcAttribute != null)
+                {
+                    string processedUrl = AddProtocolIfMissing(srcAttribute.Value);
+                    processedUrl = processedUrl.Replace("&amp;", "&");
+                    processedVedioLinks.Add((processedUrl, 1));
+                    return processedVedioLinks;
+                }
+            }
+
+            LogMessage($"未找到 id 为 '{elementId}' 的 vedio 元素。未找到视频链接", LogLevel.Error);
+            return processedVedioLinks;
+        }
+
+        public List<(string Url, int Index)> ExtractImageLinksFromDiv(string targetDivClassName)
+        {
+            var processedImageLinks = new List<(string Url, int Index)>();
+            int currentIndex = 1;
+            List<string> suffixesToRemove = new List<string>
+            {
+                "_90x90q30.jpg_.webp",
+                "_q50.jpg_.webp"
+                // 可以根据需要添加更多可能的结尾
+            };
+
+            var doc = new HtmlDocument();
+            doc.LoadHtml(fullHtmlContent);
+
+            var targetDivs = doc.DocumentNode.SelectNodes($"//div[contains(@class, '{targetDivClassName}')]");
+
+            if (targetDivs == null || !targetDivs.Any())
+            {
+                LogMessage($"未找到 class 为 '{targetDivClassName}' 的 div 元素。", LogLevel.Error);
+
+                return processedImageLinks;
+            }
+
+            Regex regex = new Regex(ImageSrcRegexPattern, RegexOptions.IgnoreCase);
+
+            foreach (var divNode in targetDivs)
+            {
+                string divHtml = divNode.InnerHtml;
+                MatchCollection matches = regex.Matches(divHtml);
+
+                foreach (Match match in matches)
+                {
+                    if (match.Groups.Count > 1)
+                    {
+                        string src = match.Groups[1].Value;
+                        string processedUrl = AddProtocolIfMissing(src);
+
+                        if (!string.IsNullOrEmpty(processedUrl))
+                        {
+                            // 使用 LINQ 查找所有后缀的 LastIndexOf，并选择最大的那个索引
+                            int bestMatchIndex = suffixesToRemove
+                                .Select(suffix => processedUrl.LastIndexOf(suffix)) // 获取每个后缀的 LastIndexOf 结果
+                                .Where(index => index != -1) // 过滤掉未找到的 (-1)
+                                .DefaultIfEmpty(-1) // 如果没有找到任何后缀，则默认为 -1
+                                .Max(); // 找到最大的索引
+
+                            processedUrl = bestMatchIndex != -1
+                                ? processedUrl.Substring(0, bestMatchIndex)
+                                : processedUrl; // 如果 bestMatchIndex 是 -1，说明没有匹配到，url 不变
+
+                            processedImageLinks.Add((processedUrl, currentIndex++));
+                        }
+                    }
+                }
+            }
+
+            return processedImageLinks;
+        }
+
+        private string AddProtocolIfMissing(string url)
+        {
+            if (string.IsNullOrEmpty(url))
+            {
+                return null;
+            }
+
+            if (url.StartsWith("//"))
+            {
+                return $"{defaultProtocol}:{url}";
+            }
+
+            if (Uri.TryCreate(url, UriKind.Absolute, out _))
+            {
+                return url;
+            }
+
+            LogMessage($"    发现相对路径 URL '{url}'，但未提供 baseUrl，无法完整处理。", LogLevel.Warning);
+            return null;
+        }
+
+        public async Task DownloadVedioParallelAsync(List<(string Url, int Index)> InfoList)
+        {
+            if (InfoList.Any())
+            {
+                string categoryPath = Path.Combine(downloadDirectory, "视频");
+                if (!Directory.Exists(categoryPath))
+                {
+                    Directory.CreateDirectory(categoryPath);
+                }
+
+                LogMessage($"准备下载 视频 共计 {InfoList.Count} 个视频...", LogLevel.Processing);
+                var task = InfoList.Select(info => DownloadImageAsync(info.Url, info.Index, categoryPath, true));
+                await Task.WhenAll(task);
+                LogMessage("所有视频下载完成", LogLevel.Success);
+            }
+            else
+            {
+                LogMessage("没有找到有效的视频链接进行下载", LogLevel.Error);
+            }
+        }
+
+        public async Task DownloadImagesParallelAsync(List<(string Url, int Index)> imageInfoList, string category)
+        {
+            if (imageInfoList.Any())
+            {
+                string categoryPath = Path.Combine(downloadDirectory, category);
+                if (!Directory.Exists(categoryPath))
+                {
+                    Directory.CreateDirectory(categoryPath);
+                }
+
+                LogMessage($"准备下载 {category} 共计 {imageInfoList.Count} 张图片...", LogLevel.Processing);
+
+                var task = imageInfoList.Select(imageInfo => DownloadImageAsync(imageInfo.Url, imageInfo.Index, categoryPath, false));
+                await Task.WhenAll(task);
+
+                LogMessage("所有图片下载完成", LogLevel.Success);
+                /*
+                if (category == "主图")
+                {
+                    LogMessage("开始转换主图格式", LogLevel.Processing);
+                    var webpFiles = Directory.GetFiles(categoryPath, "*.webp");
+                    foreach (var webpFile in webpFiles)
+                    {
+                        string jpgFilePath = Path.ChangeExtension(webpFile, ".jpg");
+                        Convert(webpFile, jpgFilePath);
+                    }
+                }
+                */
+            }
+            else
+            {
+                LogMessage("没有找到有效的图片链接进行下载", LogLevel.Error);
+            }
+        }
+
+        private async Task DownloadImageAsync(string imageUrl, int index, string downPath, bool isvedio)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    var response = await client.GetAsync(imageUrl);
+                    response.EnsureSuccessStatusCode();
+
+                    string extension = Path.GetExtension(imageUrl);
+                    if (string.IsNullOrEmpty(extension))
+                    {
+                        var contentType = response.Content.Headers.ContentType?.MediaType;
+                        if (!string.IsNullOrEmpty(contentType))
+                        {
+                            switch (contentType.ToLower())
+                            {
+                                case "image/jpeg":
+                                    extension = ".jpg";
+                                    break;
+
+                                case "image/png":
+                                    extension = ".png";
+                                    break;
+
+                                case "image/gif":
+                                    extension = ".gif";
+                                    break;
+
+                                case "image/bmp":
+                                    extension = ".bmp";
+                                    break;
+
+                                case "image/webp":
+                                    extension = ".webp";
+                                    break;
+
+                                case "video/mp4":
+                                    extension = ".mp4";
+                                    break; // 增加对 MP4 的支持
+
+                                default:
+                                    extension = ".jpg";
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            extension = ".jpg";
+                        }
+                    }
+
+                    if (isvedio)
+                    {
+                        extension = ".mp4";
+                    }
+
+                    var fileName = $"{index:D2}{extension}";
+                    var filePath = Path.Combine(downPath, fileName);
+
+                    using (var stream = await response.Content.ReadAsStreamAsync())
+                        using (var fileStream = File.Create(filePath))
+                        {
+                            await stream.CopyToAsync(fileStream);
+                        }
+
+                    LogMessage($"已下载: {fileName} (来自: {imageUrl})", LogLevel.Success);
+                }
+                catch (HttpRequestException ex)
+                {
+                    LogMessage($"下载失败: {imageUrl} - {ex.Message}", LogLevel.Error);
+                }
+                catch (Exception ex)
+                {
+                    LogMessage($"处理链接 {imageUrl} 时发生错误: {ex.Message}", LogLevel.Error);
+                }
+            }
+        }
+
+        private async void btnDown_Click(object sender, EventArgs e)
+        {
+            if (rtbHtml.Text == "")
+            {
+                LogMessage("窗口未粘贴源代码，请重试！", LogLevel.Error);
+                return;
+            }
+
+            btnDown.Enabled = false;
+            fullHtmlContent = rtbHtml.Text;
+
+            string downloadFolder = "TargetedDownloadsFromFile";
+            string protocol = "https"; // 默认协议
+            downloadDirectory = Path.Combine(txtOutputFolder.Text, GetIDFromUrl());
+            if (!Directory.Exists(downloadDirectory))
+            {
+                Directory.CreateDirectory(downloadDirectory);
+            }
+
+            // 2. 提取 thumbnails--v976to2t 中的图片链接
+            LogMessage("正在提取主图清单", LogLevel.Processing);
+            List<(string Url, int Index)> thumbnailImages = ExtractImageLinksFromDiv("thumbnails--v976to2t");
+
+            // 3. 提取 descV8-container 中的图片链接
+            LogMessage("正在提取详情图清单", LogLevel.Processing);
+            List<(string Url, int Index)> descImages = ExtractImageLinksFromDiv("descV8-container");
+
+            // 4. 提取 content--DIGuLqdf 中的图片链接
+            LogMessage("正在提取SKU图清单", LogLevel.Processing);
+            List<(string Url, int Index)> SKUImages = ExtractImageLinksFromDiv("content--DIGuLqdf");
+
+            // 4. 提取 videox-video-el 中的视频链接
+            LogMessage("正在提取视频链接", LogLevel.Processing);
+            List<(string Url, int Index)> Vedios = ExtractVedioLinksFromDiv("videox-video-el");
+
+            // 5. 下载所有图片
+            await DownloadImagesParallelAsync(thumbnailImages, "主图");
+            await DownloadImagesParallelAsync(SKUImages, "SKU图");
+            await DownloadImagesParallelAsync(descImages, "详情图");
+            await DownloadVedioParallelAsync(Vedios);
+            LogMessage("所有操作已完成！", LogLevel.Success);
+            rtbHtml.Text = "";
+            btnDown.Enabled = true;
+        }
+
+        private void btnFloder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                // 设置对话框的初始路径
+                if (!string.IsNullOrEmpty(txtOutputFolder.Text) && Directory.Exists(txtOutputFolder.Text))
+                {
+                    fbd.SelectedPath = txtOutputFolder.Text;
+                }
+                else
+                {
+                    // 否则，使用我的文档作为默认
+                    fbd.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                }
+
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    txtOutputFolder.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void txtOutputFolder_TextChanged(object sender, EventArgs e)
+        {
+            LogMessage($"图包输出文件夹已设置为: \"{txtOutputFolder.Text}\"", LogLevel.Info);
+        }
+
+        // 日志输出函数
+        private enum LogLevel
+        {
+            Info,
+            Warning,
+            Error,
+            Success,
+            Processing
+        }
+    }
+}
